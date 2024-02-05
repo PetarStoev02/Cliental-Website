ec66f125da034b506cbed36a48163f813f1e6adf
Петър Стоев <petarstoev0505@gmail.com>
2024-02-05 13:56:58 +0200

    feat: New Nav Bar

--- Cliental/src/components/NavBar/NavBar.jsx
+++ Cliental/src/components/NavBar/NavBar.jsx
@@ -1,10 +1,11 @@
-import NavButton from '../NavButton/NavButton';
-import * as ROUTES from '../../common/routes';
-import {SERVICES, PORTFOLIO, CONTACTS} from '../../common/constants';
-import './NavBar.css';
-import React, {useState} from 'react';
-import Logo from '../Logo/Logo';
-import ContactButton from '../ContactButton/ContactButton';
+import NavButton from "../NavButton/NavButton";
+import * as ROUTES from "../../common/routes";
+import { SERVICES, PORTFOLIO, CONTACTS } from "../../common/constants";
+import "./NavBar.css";
+import React, { useEffect, useState } from 'react'
+import Logo from "../Logo/Logo";
+import ContactButton from "../ContactButton/ContactButton";
+import SocialMediaIcons from "../SocialMediaIcons/SocialMediaIcons";
 
 // eslint-disable-next-line react/prop-types
 const NavBar = () => {
@@ -12,42 +13,70 @@ const NavBar = () => {
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
+  const [show, setShow] = useState(true)
+    const controlNavbar = () => {
+        if (window.scrollY > 100) {
+            setShow(false)
+        } else {
+            setShow(true)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', controlNavbar)
+        return () => {
+            window.removeEventListener('scroll', controlNavbar)
+        }
+    }, [])
+
   return (
-    <div className={click ? 'nav-bar active' : 'nav-bar'}>
-      <Logo onClick={closeMobileMenu} />
-      <div className="menu-icon" onClick={handleClick}>
-        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+    <div className={click ? "nav-bar active" : "nav-bar"}>
+      <div className={`${show && 'nav-top-side'}`}>
+        <SocialMediaIcons/>
+        <div className="languish">
+          <span>BG</span>
+          <span>|</span>
+          <span>EN</span>
+        </div>
       </div>
-      <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-        <li className="nav-item">
-          <NavButton
-            className="nav-links"
-            route={ROUTES.SERVICES_PAGE}
-            onClick={closeMobileMenu}
-          >
-            {SERVICES}
-          </NavButton>
-        </li>
-        <li className="nav-item">
-          <NavButton
-            className="nav-links"
-            route={ROUTES.PORTFOLIO_PAGE}
-            onClick={closeMobileMenu}
-          >
-            {PORTFOLIO}
-          </NavButton>
-        </li>
-        <li className="nav-item">
-          <NavButton
-            className="nav-links"
-            route={ROUTES.CONTACTS_PAGE}
-            onClick={closeMobileMenu}
-          >
-            {CONTACTS}
-          </NavButton>
-        </li>
-      </ul>
-      <ContactButton />
+      <div className={`${show && 'nav-divider'}`}></div>
+      <div className="nav-bottom-side">
+        {" "}
+        <Logo onClick={closeMobileMenu} />
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"} />
+        </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <NavButton
+              className="nav-links"
+              route={ROUTES.SERVICES_PAGE}
+              onClick={closeMobileMenu}
+            >
+              {SERVICES}
+            </NavButton>
+          </li>
+          <li className="nav-item">
+            <NavButton
+              className="nav-links"
+              route={ROUTES.PORTFOLIO_PAGE}
+              onClick={closeMobileMenu}
+            >
+              {PORTFOLIO}
+            </NavButton>
+          </li>
+          <li className="nav-item">
+            <NavButton
+              className="nav-links"
+              route={ROUTES.CONTACTS_PAGE}
+              onClick={closeMobileMenu}
+            >
+              {CONTACTS}
+            </NavButton>
+          </li>
+        </ul>
+        <ContactButton />
+      </div>
     </div>
   );
 };
